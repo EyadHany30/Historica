543ec4b3c816efc79d059a60ba9c229bb498ed68
Eyad_hanyy <eyadhany30@gmail.com>
2024-04-20 15:19:15 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Historica());
+}
+
+class Historica extends StatelessWidget {
+  const Historica({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
